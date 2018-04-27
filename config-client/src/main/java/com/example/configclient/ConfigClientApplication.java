554ea092401d1213d87e30ab14e3c9d2ef0806d3
chenjian <chenjian@suneee.com>
2018-04-27 17:51:25 +0800

    分布式配置中心

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -2,10 +2,12 @@ package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.example")
+@EnableEurekaClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
